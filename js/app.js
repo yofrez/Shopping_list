6e002d6627451dad3cb16c596bbc7ba04c649a15
yofrez <yohanes.frezgi@gmail.com>
2016-01-20 16:27:18 -0500

    ”finished”

--- js/app.js
+++ js/app.js
@@ -1,11 +1,26 @@
 $(document).ready(function(){
 $('form').submit(function(){
 	var value = $('input[type=text]').val();
-	$('ul').append('<li>'+value+'</li>');
+	$('ul').append("<li> <input type='checkbox'> "+value+"</li>");
 	return false;
 });
 
-$('ul').on("click",'li',  function() {
-	$(this).remove();
-	});
+$('ul').on("click",'input', function(){
+// check if checkbox is selected 
+	if ($(this).prop( "checked")){ 
+// if check box is check select parent element and add strike through css 
+		$(this).parent().css("text-decoration", "line-through")
+// if checkbox is not check remove strike through of css of parent. 
+	}
+	else { $(this).parent().css("text-decoration", "none");
+	}
+
 })
+
+	// $('ul').on("click",'li',  function() {
+	// $(this).remove();
+	//  });
+  
+
+
+  })
